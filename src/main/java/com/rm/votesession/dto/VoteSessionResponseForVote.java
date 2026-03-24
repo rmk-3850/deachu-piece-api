a9903761d1ecc4a796fabea4493937550a4c56eb
rmk-3850 <qop589@naver.com>
2026-03-24 09:26:08 +0900

    응답 시간 형식 zone포함

--- src/main/java/com/rm/votesession/dto/VoteSessionResponseForVote.java
+++ src/main/java/com/rm/votesession/dto/VoteSessionResponseForVote.java
@@ -3,6 +3,7 @@ package com.rm.votesession.dto;
 import java.time.LocalDateTime;
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.rm.piece.dto.CandidateWithPieceDto;
 import com.rm.votesession.VoteType;
 
@@ -10,7 +11,9 @@ public record VoteSessionResponseForVote(
     Long id,
     String term,
     VoteType voteType,
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     LocalDateTime endDateTime,
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'")
     LocalDateTime servernow,
     List<CandidateWithPieceDto> list
 ) {
